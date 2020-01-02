6ccefcde65779f5592ab72055757c4774b321fd8
Dioris Diaz <diorid@gmail.com>
2020-01-02 04:44:51 -0500

    I understand it a little more

---
+++ psuedo.md
@@ -0,0 +1,13 @@
+* create a nav bar 
+- have a dashboard 
+
+
+
+the entire url: http://api.openweathermap.org/data/2.5/weather?q=Philadelphia,us&apikey=741723edae656a0451e3776abc421da5
+
+the api key:"741723edae656a0451e3776abc421da5
+
+the url wihout the api key: api.openweathermap.org/data/2.5/weather?q=Philadelphia,us&apikey="
+
+
+source for side bar: https://bootstrapious.com/p/bootstrap-sidebar
---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+// Set up variables 
+// ==============================================
+    var queryTerm = "";
+    var auKeys = "741723edae656a0451e3776abc421da5";
+
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + "&apikey=" + auKeys;
+
+
+
+// Functions
+// ================================================
+
+// Main Process
+// =================================================
+
+
+});
